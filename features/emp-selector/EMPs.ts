b7fc1cdaef8354a95eddb0f03f8c14b0d9a7251d
smb2796 <sean.brown2796@gmail.com>
2021-03-01 14:26:52 -0500

    testing ustonks on kovan

--- features/emp-selector/EMPs.ts
+++ features/emp-selector/EMPs.ts
@@ -1,8 +1,11 @@
 export const EMPs: { [networkId: number]: string[] } = {
   1: [
     "0x516f595978d87b67401dab7afd8555c3d28a3af4", // uGAS-JAN21 Mainnet
+    "0xEAA081a9fad4607CdF046fEA7D4BF3DfEf533282",
+    "0xfA3AA7EE08399A4cE0B4921c85AB7D645Ccac669",
   ],
   42: [
     "0xb3Afc977e93Be003b6e973cdA81dab7803Bf2762", // uGas-JAN Kovan
+    "0x90d23f4f400c2c71a3499bd6a59ed21027a11b84",
   ],
 };
